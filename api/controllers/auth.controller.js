9eb6e7806e3f9d1c7ec8d39323cb76b5e0cdf586
Joshua Biju Korah <joshuabijukorah@gmail.com>
2025-02-01 15:28:45 +0530

    db connection and newUser creation

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,9 +1,21 @@
 import bcrypt from "bcrypt";
+import prisma from "../lib/prisma.js";
+
 export const register = async (req, res) => {
   const { username, email, password } = req.body;
 
   const hashedPassword = await bcrypt.hash(password, 10);
   console.log(hashedPassword);
+
+  const newUser = await prisma.user.create({
+    data: {
+      username,
+      email,
+      password: hashedPassword,
+    },
+  });
+  console.log(newUser);
+  res.send(201);
 };
 
 export const login = (req, res) => {};
